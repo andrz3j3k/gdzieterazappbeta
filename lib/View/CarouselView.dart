4915df0d3df31c105d4fb2a8c16051c961a6f4ab
Dominik <dominikgasior03@gmail.com>
2022-08-10 12:29:24 +0200

    Adding feature in CarouselView. Scroll and size

--- lib/View/CarouselView.dart
+++ lib/View/CarouselView.dart
@@ -7,11 +7,54 @@ class CarouselView extends StatefulWidget {
   State<CarouselView> createState() => _CarouselView();
 }
 
+MyCard myCard = MyCard();
+
 class _CarouselView extends State<CarouselView> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('karty'),
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: ListView.builder(
+        physics: const PageScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: myCard.listCard.length,
+        itemBuilder: (context, index) {
+          myCard.indexAmount = index;
+          return widgetCard();
+        },
+      ),
     );
   }
+}
+
+class MyCard {
+  int indexAmount = 0;
+  var listCard = [
+    'https://nadzory-archeologiczne.pl/wp-content/uploads/2018/03/Przemyśl-panorama-z-wieży-archikatedry.jpg',
+    'https://nadzory-archeologiczne.pl/wp-content/uploads/2018/03/Przemyśl-panorama-z-wieży-archikatedry.jpg',
+    'https://nadzory-archeologiczne.pl/wp-content/uploads/2018/03/Przemyśl-panorama-z-wieży-archikatedry.jpg',
+    'https://www.polska.travel/images/pl-PL/glowne-miasta/przemysl/przemysl_rynek_1170.jpg',
+    'https://nadzory-archeologiczne.pl/wp-content/uploads/2018/03/Przemyśl-panorama-z-wieży-archikatedry.jpg',
+    'https://www.polska.travel/images/pl-PL/glowne-miasta/przemysl/przemysl_rynek_1170.jpg',
+    'https://nadzory-archeologiczne.pl/wp-content/uploads/2018/03/Przemyśl-panorama-z-wieży-archikatedry.jpg',
+    'https://www.polska.travel/images/pl-PL/glowne-miasta/przemysl/przemysl_rynek_1170.jpg',
+    'https://nadzory-archeologiczne.pl/wp-content/uploads/2018/03/Przemyśl-panorama-z-wieży-archikatedry.jpg',
+    'https://www.polska.travel/images/pl-PL/glowne-miasta/przemysl/przemysl_rynek_1170.jpg',
+  ];
+}
+
+Widget widgetCard() {
+  return Center(
+    child: SizedBox(
+      width: 350,
+      height: 450,
+      child: Card(
+        color: Colors.black,
+        child: Image.network(
+          myCard.listCard[myCard.indexAmount],
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+  );
 }
