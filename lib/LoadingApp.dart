a0571d4ab9728a15569716860cc2c7813059e92b
Dominik <dominikgasior03@gmail.com>
2022-10-17 17:53:22 +0200

    Dynamic www address on loadingPage

--- lib/LoadingApp.dart
+++ lib/LoadingApp.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:travelon/Style/ScaffoldStyle.dart';
 import 'package:travelon/Widget/NoConnectionInternet.dart';
+import 'package:travelon/Widget/ProgressIndicator.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+
+import 'Models/MainPageModel/MainText.dart';
 
 class LoadingPage extends StatefulWidget {
-  LoadingPage({super.key, required this.text, required this.type});
+  LoadingPage({
+    super.key,
+    required this.text,
+    required this.type,
+  });
   String text;
   String type;
   @override
@@ -12,46 +22,66 @@ class LoadingPage extends StatefulWidget {
 }
 
 class _LoadingPageState extends State<LoadingPage> {
+  Future<MainText> fetchwww() async {
+    //pobranie strony WWW
+    final response = await http.get(
+      Uri.parse('https://gdzieterazapp.pl/api/settings/upgrade'),
+    );
+
+    // Use the compute function to run parsePhotos in a separate isolate.
+    return MainText.fromJson(jsonDecode(response.body));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: widget.type == "Brak aktualizacji"
-          ? Center(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.system_update,
-                      size: 40,
-                      color: whatIsDarkMode
-                          ? darkColorText
-                          : themeLight.primaryColor,
+          ? FutureBuilder(
+              future: fetchwww(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return NoConnectionInternet();
+                } else if (snapshot.hasData) {
+                  return Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.system_update,
+                            size: 40,
+                            color: whatIsDarkMode
+                                ? darkColorText
+                                : themeLight.primaryColor,
+                          ),
+                          const Padding(padding: EdgeInsets.only(bottom: 10)),
+                          Text(
+                            widget.text,
+                            textAlign: TextAlign.center,
+                          ),
+                          const Padding(padding: EdgeInsets.only(top: 20)),
+                          ElevatedButton(
+                            onPressed: () async {
+                              var uri = Uri.parse(snapshot.data!.text);
+                              if (await canLaunchUrl(uri)) {
+                                await launchUrl(uri);
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: whatIsDarkMode
+                                    ? backgroundColorButtonDark
+                                    : themeLight.primaryColor),
+                            child: const Text("Zaaktualizuj mnie!"),
+                          ),
+                        ],
+                      ),
                     ),
-                    const Padding(padding: EdgeInsets.only(bottom: 10)),
-                    Text(
-                      widget.text,
-                      textAlign: TextAlign.center,
-                    ),
-                    const Padding(padding: EdgeInsets.only(top: 20)),
-                    ElevatedButton(
-                      onPressed: () async {
-                        var uri = Uri.parse(
-                            "https://play.google.com/store/apps/details?id=com.kitkagames.fallbuddies&gl=PL");
-                        if (await canLaunchUrl(uri)) {
-                          await launchUrl(uri);
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: whatIsDarkMode
-                              ? backgroundColorButtonDark
-                              : themeLight.primaryColor),
-                      child: const Text("Zaaktualizuj mnie!"),
-                    ),
-                  ],
-                ),
-              ),
+                  );
+                } else {
+                  return const CircularProgressIndicatorCustom();
+                }
+              },
             )
           : const Center(
               child: NoConnectionInternet(),
